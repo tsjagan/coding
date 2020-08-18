b4a2522cff2d8c654f602a2b3ff978c7d4c27a43
Jagannath TImma <tsjagan@gmail.com>
2020-08-17 21:48:26 -0700

    DP make palindrome

--- leetcode/make_pallindrome.cpp
+++ leetcode/make_pallindrome.cpp
@@ -47,6 +47,35 @@ bool edits_to_palindrome(string& str, int k) {
     return _edits_to_palindrome(str, 0, str.size() - 1, k);
 }
 
+bool edits_to_palindrome_dp(const string& str, int k) {
+    int n = str.size();
+    vector<vector<int>> dp(n, vector<int>(n, k + 1));
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = i; j < n; j++) {
+            if (i == j) {
+                dp[i][j] = 0;
+            } else if (i + 1 == j) {
+                dp[i][j] = (str[i] == str[j] ? 0 : 1);
+            } else {
+                if (str[i] == str[j]) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+    }
+
+    return (dp[0][n - 1] <= k);
+}
+
+// <str> is palindrome break and return
+// all strings for length 1 is true
+// otherwise:
+//    - (str[i] == str[j] and dp[k][i+1][j-1] == true) or
+//    - (dp[k-1][i][j-1] == true) or
+//    - (dp[k-1][i+1][j] == true) or
 int main(void) {
     string str;
     int k;
@@ -57,6 +86,9 @@ int main(void) {
     cin >> k;
 
     auto ret = time_this(edits_to_palindrome, str, k);
+    cout << ret.first << ":" << ret.second.count() << endl;
+
+    ret = time_this(edits_to_palindrome_dp, str, k);
     cout << ret.first << ":" << ret.second.count() << endl;
     return 0;
 }
